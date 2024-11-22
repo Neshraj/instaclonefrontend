f01babde3c3443d193522aad48381304afca5ce1
S NESHRAJ <144615476+Neshraj@users.noreply.github.com>
2024-11-22 14:49:59 +0530

    Update updatedata.jsx

--- src/components/updatedata.jsx
+++ src/components/updatedata.jsx
@@ -28,7 +28,7 @@ function Updatedata() {
   const [name, setName] = useState('---');
   const [password, setPassword] = useState('---');
   const [cpassword, setCpassword] = useState('---');
-  const mainServerUrl = 'https://instacloneserver-00mi.onrender.com';
+  const mainServerUrl = 'https://instacloneserver-raj1.onrender.com';
 
   useEffect(() => {
 
